2572133d8f9d16c907f4b02d1b8729dcf4bddbe1
Unknown <punto.damar2@gmail.com>
2019-01-13 23:59:49 +0700

    export

---
+++ docs/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1547398574,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/background.png",
+		"images/weapon-sheet0.png",
+		"images/playerbullet-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/badguy-sheet0.png",
+		"images/enemyspawner-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
